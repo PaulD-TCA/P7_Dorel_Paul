bf972c5bc7455913232e3dc309920b4f2417d5cb
Dorel <dorel.paul@gmail.com>
2020-08-31 00:09:03 +0200

    placeposition improved

---
+++ appgrandpy/modules/placeposition.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+# coding: utf-8
+import requests
+import json
+
+class Position:
+    """
+    Send a request to Google Maps to find an adress place and his position.
+    """
+    def adress_position(self, words_keeped, config):
+        """
+        @type words_keeped: List.
+        @param words_keeped: Few importants words about a place or a topic.
+        @type usergooglekey: String.
+        @param usergooglekey: A user google API key.
+        @return: An adress and his position or a sentance
+        if noting is returned by Google Maps.
+        """
+        try:
+            api_url = "https://maps.googleapis.com/maps/api/geocode/json?"
+            search_params = {"address": words_keeped, "key": config.USER_GOOGLE_KEY}
+            resp = requests.get(api_url, search_params)
+            geocode_result = resp.json()
+
+            adress = geocode_result["results"][0]["formatted_address"]
+            latlong = geocode_result["results"][0]["geometry"]["location"]
+
+            return adress, latlong
+        except:
+            return "Je ne me rapelle pas où cela ce trouve.", {"lat": 0, "lng": 0}
